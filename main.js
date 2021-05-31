1497f33c038a32ea228b1f816bb43ca74802524a
Matias Fuentes <matiasf.cortez@gmail.com>
2021-05-31 00:23:52 -0400

    Generacion de cajas

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const pokecontainer = document.querySelector('.poke_container');
+const selectGen = document.querySelector('#select_gen');
+const btn_gen = document.querySelector('.btn_generar');
+const btn_buscar = document.querySelector('.btn_buscar');
+const pokeBox = document.getElementsByClassName(".poke_box");
 
-function getPokemons(){
- for(i=0; i<=151;i++){
-
-    fetch('https://pokeapi.co/api/v2/pokemon/'+i)
+function getPokemons(count,limit){
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/'+count)
     .then(data => data.json())
     .then(data => {
 
@@ -11,16 +14,41 @@ function getPokemons(){
         
     template = 
     `<div class="poke_box">
-        <h2 class="poke_name">${data.name}</h2>
+        <h2 class="poke_name">${FirstLetterUpperCase(data.name)}</h2>
         <img class="poke_img" src="${data.sprites.front_default}"> 
     </div>`
     
     div.innerHTML = template;
 
     pokecontainer.append(div.firstElementChild);
+    if(count>=1 && limit>count){
+
+        getPokemons(count+1,limit)
+    
+    }
 
     })
- }
-   
+
 }
-getPokemons()
+
+function FirstLetterUpperCase(str){
+    return str[0].toUpperCase()+str.slice(1);
+}
+
+btn_gen.addEventListener('click', ()=>{
+    pokecontainer.innerHTML= '';
+
+    values = selectGen.value.split(",");
+    values[0] = parseInt(values[0])
+    values[1] = parseInt(values[1])
+
+    getPokemons(values[0],values[1])
+    
+})
+
+btn_buscar.addEventListener('click', ()=>{
+
+const pokeBox = document.getElementsByClassName("poke_box");
+
+
+})
